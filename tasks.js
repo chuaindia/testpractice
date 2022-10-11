21d6a3b5473e7e467992fde95c4011d7c60262c8
Tanusri Ghosh <tghosh25@gmail.com>
2022-10-11 21:02:24 +0530

    Update tasks.js
    Task 3 : Introduction of simple calculations

--- tasks.js
+++ tasks.js
@@ -23,5 +23,15 @@ const reverseString = (string) => string.split(" ").reverse().join(" ");
 
 // Task 3
 
+class calculator {
+    add = (number1, number2) => number1 + number2;
+    subtract = (number1, number2) => number1 - number2;
+    multiply = (number1, number2) => number1 * number2;
+    divide = (number1, number2) => number1 / number2;
+}
+const Calculator = new calculator;
+
+// Task 4
+
 
 
