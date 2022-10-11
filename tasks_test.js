7f6060b1e1e5b1cbb39867fb4a9491d92b4845a8
Tanusri Ghosh <tghosh25@gmail.com>
2022-10-11 21:14:10 +0530

    Test for calculations added
    Simple Add, Subtract, Multiply and Divide operations are incorporated using "describe".

--- tasks_test.js
+++ tasks_test.js
@@ -1,6 +1,7 @@
 import {
     stringLength,
     reverseString,
+    Calculator,
        } from "./tasks.js";
 
 test("String Length:", () => {
@@ -22,3 +23,28 @@ test("String length more than 10", () => {
 test("Reversed String:", () => {
   expect(reverseString("chuaindia")).toBe("aidniauhc");
 });
+
+describe("Calculator functions:", () => {
+   test("Add numbers:", () => {
+       expect(Calculator.add(1, 2)).toBe(3);
+       expect(Calculator.add(3, 4)).toBe(7);
+       expect(Calculator.add(5, 6)).toBe(11);
+   });
+    test("Subtract numbers:", () => {
+        expect(Calculator.subtract(1, 2)).toBe(-1);
+        expect(Calculator.subtract(23, 14)).toBe(9);
+        expect(Calculator.subtract(8, 6)).toBe(2);
+    });
+    test("Multiply numbers:", () => {
+        expect(Calculator.multiply(1, 2)).toBe(2);
+        expect(Calculator.multiply(3, 4)).toBe(12);
+        expect(Calculator.multiply(5, 6)).toBe(30);
+    });
+    test("Divide numbers:", () => {
+        expect(Calculator.divide(12, 2)).toBe(6);
+        expect(Calculator.divide(32, 4)).toBe(8);
+        expect(Calculator.divide(42, 6)).toBe(7);
+    });
+});
+    
+    
