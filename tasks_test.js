f22b07b877940ea801f16757c074fec5297e1f03
Tanusri Ghosh <tghosh25@gmail.com>
2022-10-11 20:49:20 +0530

    Update tasks_test.js

--- tasks_test.js
+++ tasks_test.js
@@ -1,5 +1,17 @@
 import { stringLength } from "./tasks.js";
 
-TextDecoderStream("String Length:", () => {
+test("String Length:", () => {
     expect(stringLength("Hello World!")).toBe(12);
+});
+
+test("String length less than 1", () => {
+    expect(() => stringLength("")).toThrowError(
+        "Failed: String length must not be less than 1 character"
+    );
+});
+
+test("String length more than 10", () => {
+    expect(() => stringLength("Hi! I am Tanusri from India")).toThrowError(
+        "Failed: String length must not be more than 10 caracters"
+    );
 });
