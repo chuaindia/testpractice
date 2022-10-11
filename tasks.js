6ea671b133225084898693fe9c5e9cd75d31cedd
Tanusri Ghosh <tghosh25@gmail.com>
2022-10-11 20:43:42 +0530

    Update tasks.js
    Implementation of conditional check of stringlength

--- tasks.js
+++ tasks.js
@@ -1,7 +1,16 @@
+// Tasks
+
 const stringLength = (string) =>
 {
     const stringLength = string.length;
+    try {
+        if(stringLength<1) {
+            throw new Error ("String Length must be greater than or equal to 1");
+        }
+        if(stringLength>10) {
+            throw new Error ("String Length must be less than or equal to 10");
+        }
+    } 
     return stringLength;
-
 }
 
